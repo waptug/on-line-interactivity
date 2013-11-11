7e3c4c03be0a15f12ef70220d683faff37a2c6c3
Michael McGinn <waptug@gmail.com>
2013-11-10 16:50:25 -0800

    Create index.php
    Created index.php to begin tracking changes to the main resource page.

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+
+<head>
+<title>WapTug.Org - WebIDE</title>
+    
+    <meta charset="utf-8">
+    <meta name="description" content="Web Based Integrated Development Environment For Collaborative Group Code Development. Join our web application programming technology user group and learn how to create web applications. Use our CODIAD on-line web application integrated development environment to code your open source applications and share them with the world.">
+	<meta name="keywords" content="CODIAD,HTML,HTML5,CSS,CSS3,XML,JavaScript,PHP,Michael Scott McGinn,WapTug,Web Applications Programming Technology Users Group,On-Line-Interactivity.Com,GeekZoneBooks.Com,WapTug.Org">
+	<meta name="author" content="Michael Scott McGinn, CODIAD Team,You">
+	<meta name="wot-verification" content="4e7ffa043aa6b183b0aa"/>
+    <link rel="stylesheet" href="WebIDE/themes/default/reset.css">
+    <link rel="stylesheet" href="WebIDE/themes/default/fonts.css">
+    <link rel="stylesheet" href="WebIDE/themes/default/screen.css">
+    <style type="text/css">
+        html { overflow: scroll; }
+        body { width: 100%; margin: 0 auto; overflow: scroll; }
+        td .icon { font-size: 30px; display: inline; padding-top: 0; margin-top: 0; }
+        p { padding: 15px 0; margin: 0; font-weight: bold; }
+        label { margin-top: 25px; }
+        #container { width: 600px; margin: 50px auto; }
+        #logo {width:600px; margin:50px auto;}
+        .item-icon { float: right; }
+    </style>
+	<!-- Google Analytics -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45577105-1', 'on-line-interactivity.com');
+  ga('send', 'pageview');
+
+</script>
+<!-- jquery menu -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	$("#hidemenu").click(function(){
+		$("#showmenu").show();
+		$("#hidemenu").hide();
+		$("#container").fadeOut("slow",.0);
+		$("#about").fadeIn("slow",.0);
+	});
+	$("#showmenu").click(function(){
+		$("#hidemenu").show();
+		$("#showmenu").hide();
+		$("#container").fadeIn("slow",.0);
+		$("#about").fadeOut("slow",.0);
+	});
+$("#container").hide();
+$("#hidemenu").hide();
+});
+</script>
+  <!-- end jquery menu -->
+  
+</head>
+
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=103650273035821";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+    <script>
+    var i18n = (function(lang) {
+        return function(word) {
+            return (word in lang) ? lang[word] : word;
+        }
+    })([])
+</script>
+<div id="logo" style="background:grey;">
+<!-- Social Buttons -->
+<div class="g-plus" data-action="share" data-height="15"></div>
+<!-- Place this code where you want the google badge to render. -->
+<a href="//plus.google.com/115522322296550875517?prsrc=3"
+   rel="publisher" target="_top" style="text-decoration:none;display:inline-block;color:#333;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
+<span style="display:inline-block;font-weight:bold;vertical-align:top;margin-right:5px; margin-top:8px;">WapTugDotOrg</span><span style="display:inline-block;vertical-align:top;margin-right:15px; margin-top:8px;">on</span>
+<img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/>
+</a>
+<!-- end of google -->
+<br/>
+<div class="fb-like" data-href="http://www.on-line-interactivity.com" data-width="450" data-layout="button_count" data-show-faces="true" data-send="true"></div>
+<script src="//platform.linkedin.com/in.js" type="text/javascript">
+				lang: en_US
+</script>
+<script type="IN/Share" data-url="http://www.on-line-interactivity.com" data-counter="right"></script>
+<a href="https://twitter.com/waptug" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @waptug</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				
+<!-- Place this tag where you want the share button to render. -->
+
+
+<!-- Place this tag after the last share tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+<!-- End Social Buttons -->
+ <img src="logo.png">
+ <textarea id="about" rows="5" readonly>
+About Us
+
+Welcome to the On-Line-Interactivity Web Based IDE Project
+
+We invite you to apply for a new account if you have an interest in learning how to create web applications and web sites.
+
+This site is powered by the CODIAD Web IDE to allow you to create projects and edit the code with out needing any client desktop programming tools.
+
+We have meetings the first Saturday of every month from 9 am to noon to share our projects and get help from other members. Members that want to use our on-line IDE tool will need to attend a meeting to have the access activated.
+
+We meet at Tullys in the Canyon Park Plaza in Bothell: 22615 Bothell Everett Hwy Bothell WA 98021
+
+This is a joint project between the following groups:
+ Eastlake Community Church Web Design and Development Growth Group
+ On-Line-Interactivity.Com  - Web Development Application IDE
+ WapTug.Org  - Web Applications Programming Technology Users Group
+ JoomlaForGeeks.Com - Joomla Users Group of Bothell
+ GeekZoneBooks.Com - Web Hosting Services (Sponsor who is paying for the hosting space)
+ </textarea>
+<div id="legallinks"><a href="http://www.waptug.org">About Us</a>|<a href="http://www.waptug.org/index.php?option=com_content&view=article&id=7">Terms of Service</a>|<a href="http://www.on-line-interactivity.com/WebIDE/index.php?">Login</a>|<a href="mailto:waptug@gmail.com">Contact Us</a>|<a href="../index.html">Stats</a>|<a href="http://on-line-interactivity.com/WapTugU/moodle">WapTug U</a>|<a href="http://bit.ly/GeekChatRoom">Support Chat</a></div>
+<div id="buttons">
+<button id="hidemenu">Hide Resource Menu</button>
+<button id="showmenu">Show Resource Menu</button>
+<button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN To WebIDE</button>
+</div>
+</div>
+
+    <div id="container">
+    <p style "font-size:10pt;">Account Functions</p>        
+    <button class="btn-left" type="submit" form="menu" formaction="http://gg.eastlakecc.com/groups/1232" value="new">Request New Membership Account</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN</button><button class="btn-right" type="submit" form="menu" formaction="../members">Current Projects Directory</button>
+	<p style="font-size:10pt;">External Web Application Hosting Resources</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://www.touchdevelop.com">Mobile</button><button class="btn-mid" type="submit" form="menu" formaction="http://bit.ly/WapTugPaaSCloud">PaaS</button><button class="btn-mid" type="submit" form="menu" formaction="https://cloud.google.com/products/app-engine">AppEngine</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.github.com">GitHub</button><button class="btn-right" type="submit" form="menu" formaction="https://chunkhost.com/r/waptug722">VPS</button>
+	<p style="font-size:10pt;">Web Application Programming Technology Training Resources</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://code.org/">Code.Org</button><button class="btn-mid" type="submit" form="menu" formaction="http://codility.com/">Codility.Com</button><button class="btn-mid" type="submit" form="menu" formaction="http://csedweek.org">Computer Science Education Week</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.w3schools.com">W3 Schools</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compileonline.com">CompileOnline</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.netlit101.com">Net Lit 101</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.codechef.com/school">CodeChef</button><button class="btn-right" type="submit" form="menu" formaction="https://www.khanacademy.org/">Khan Academy</button>
+	<p style="font-size:10pt;">Web Application Programming Tools</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://www.jsfiddle.net">jsFiddle</button><button class="btn-mid" type="submit" form="menu" formaction="http://sta.sh/muro/">Draw On-Line</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.phpclasses.org">PHP Classes</button><button class="btn-mid" type="submit" form="menu" formaction="http://doublesvsoop.sourceforge.net/">PWCT-Make Windows Applications</button><button class="btn-right" type="submit" form="menu" formaction="http://www.jsclasses.org">JavaScript Classes</button>
+	<p style="font-size:10pt;">Project Partners</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://bit.ly/EastLakeWebDevGroup">Church</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.waptug.org">Educational</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.joomlaforgeeks.com">Community</button><button class="btn-right" type="submit" form="menu" formaction="http://www.mygeekzone.net">Industry</button>
+	<p style="font-size:10pt;">Web Site Design Project Ideas</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://webdesignerwall.com/trends/single-page-portfolio-sites">Single Page Designs</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.kickstarter.com/">Fund your project</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compute.org">Seed money for your project</button><button class="btn-right" type="submit" form="menu" formaction="http://www.codiad.com">CODIAD Web IDE</button>
+
+    </div>
+<form id="menu">
+			</form> 
+            
+</body>
+</html>
