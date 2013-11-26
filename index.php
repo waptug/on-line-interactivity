83cab7999389645d1b4efd6c5c06fae554329631
Michael McGinn <waptug@gmail.com>
2013-11-26 11:11:49 -0800

    Update index.php
    Re-factored menu, added links and counter code. Added MIT blurb.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 
 <head>
-<title>WapTug.Org - WebIDE</title>
+<title>WapTug.Org - On-Line-WebIDE</title>
     
     <meta charset="utf-8">
     <meta name="description" content="Web Based Integrated Development Environment For Collaborative Group Code Development. Join our web application programming technology user group and learn how to create web applications. Use our CODIAD on-line web application integrated development environment to code your open source applications and share them with the world.">
 	<meta name="keywords" content="CODIAD,HTML,HTML5,CSS,CSS3,XML,JavaScript,PHP,Michael Scott McGinn,WapTug,Web Applications Programming Technology Users Group,On-Line-Interactivity.Com,GeekZoneBooks.Com,WapTug.Org">
 	<meta name="author" content="Michael Scott McGinn, CODIAD Team,You">
 	<meta name="wot-verification" content="4e7ffa043aa6b183b0aa"/>
-    <link rel="stylesheet" href="../../../WebIDE/themes/default/reset.css">
-    <link rel="stylesheet" href="../../../WebIDE/themes/default/fonts.css">
-    <link rel="stylesheet" href="../../../WebIDE/themes/default/screen.css">
+    <link rel="stylesheet" href="WebIDE/themes/default/reset.css">
+    <link rel="stylesheet" href="WebIDE/themes/default/fonts.css">
+    <link rel="stylesheet" href="WebIDE/themes/default/screen.css">
     <style type="text/css">
         html { overflow: scroll; }
         body { width: 100%; margin: 0 auto; overflow: scroll; }
@@ -57,7 +57,8 @@ $("#hidemenu").hide();
 </head>
 
 <body>
-
+<!-- LiveZilla Tracking Code (ALWAYS PLACE IN BODY ELEMENT) --><div id="livezilla_tracking" style="display:none"></div><script type="text/javascript">
+var script = document.createElement("script");script.async=true;script.type="text/javascript";var src = "http://www.on-line-interactivity.com/LiveZillaServer/server.php?acid=5e0d4&request=track&output=jcrpt&ovlp=MjI_&ovlc=IzczYmUyOA__&ovlct=I2ZmZmZmZg__&ovlt=TGl2ZSBIZWxwIChTdGFydCBDaGF0KQ__&ovlto=TGl2ZSBIZWxwIChMZWF2ZSBNZXNzYWdlKQ__&ovls=MQ__&nse="+Math.random();setTimeout("script.src=src;document.getElementById('livezilla_tracking').appendChild(script)",1);</script><noscript><img src="http://www.on-line-interactivity.com/LiveZillaServer/server.php?acid=5e0d4&amp;request=track&amp;output=nojcrpt&amp;ovlp=MjI_&amp;ovlc=IzczYmUyOA__&amp;ovlct=I2ZmZmZmZg__&amp;ovlt=TGl2ZSBIZWxwIChTdGFydCBDaGF0KQ__&amp;ovlto=TGl2ZSBIZWxwIChMZWF2ZSBNZXNzYWdlKQ__&amp;ovls=MQ__" width="0" height="0" style="visibility:hidden;" alt=""></noscript><!-- http://www.LiveZilla.net Tracking Code -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -74,7 +75,63 @@ $("#hidemenu").hide();
     })([])
 </script>
 <div id="logo" style="background:grey;">
-<!-- Social Buttons -->
+
+
+ <img src="logo.png">
+ <textarea id="about" rows="5" readonly>
+About Us
+
+Welcome to the On-Line-Interactivity Web Based IDE Project
+
+We invite you to apply for a new account if you have an interest in learning how to create web applications and web sites.
+
+This site is powered by the CODIAD Web IDE to allow you to create projects and edit the code with out needing any client desktop programming tools.
+This WebIDE allows you and your team to work on the same document remotely at the same time collaboratively. 
+This provides you with a fantastic tool to build working code and see it run instantly.
+
+We have meetings the first Saturday of every month from 9 am to noon to share our projects and get help from other members. Members that want to use our on-line IDE tool will need to attend a meeting to have the access activated.
+
+We meet at Tullys in the Canyon Park Plaza in Bothell: 22615 Bothell Everett Hwy Bothell WA 98021
+
+This is a joint project between the following groups:
+ Eastlake Community Church Web Design and Development Growth Group
+ On-Line-Interactivity.Com  - Web Development Application IDE
+ WapTug.Org  - Web Applications Programming Technology Users Group
+ JoomlaForGeeks.Com - Joomla Users Group of Bothell
+ GeekZoneBooks.Com - Web Hosting Services (Sponsor who is paying for the vps hosting space)
+ </textarea>
+<div id="legallinks"><a href="http://www.waptug.org">About Us</a>|<a href="http://www.waptug.org/index.php?option=com_content&view=article&id=7">Terms of Service</a>|<a href="../index.html">Stats</a>|<a href="http://on-line-interactivity.com/WapTugU/moodle">WapTug U</a>|<a href="http://waptug.eduongo.com/v2/">WapTug U Academy</a>|<a href="http://bit.ly/GeekChatRoom">Support Chat</a>|<a href="http://www.on-line-interactivity.com/WebIDE/workspace/nodejsircchat/nodejsircchat.html">IRC Links</a>
+</div>
+<div id="buttons">
+<button id="hidemenu">Hide Resource Menu</button>
+<button id="showmenu">Show Resource Menu</button>
+<button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN To WebIDE</button>
+
+<button class="btn-mid" type="submit" form="menu" formaction="http://gg.eastlakecc.com/groups/1232">Join Us</button>
+<button class="btn-mid" type="submit" form="menu" formaction="http://waptug.github.io/on-line-interactivity/">Fork On GitHub</button>
+
+
+</div>
+
+<iframe width="560" height="315" src="//www.youtube.com/embed/dU1xS07N-FA" frameborder="0" allowfullscreen></iframe>
+</div>
+
+    <div id="container">
+    <p style "font-size:10pt;">Account Functions</p>        
+    <button class="btn-left" type="submit" form="menu" formaction="http://gg.eastlakecc.com/groups/1232" value="new">Request New Membership Account</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN</button><button class="btn-right" type="submit" form="menu" formaction="../members">Current Projects Directory</button>
+	<p style="font-size:10pt;">Web Application Hosting Resources</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://www.touchdevelop.com">Mobile</button><button class="btn-mid" type="submit" form="menu" formaction="http://bit.ly/WapTugPaaSCloud">PaaS</button><button class="btn-mid" type="submit" form="menu" formaction="https://cloud.google.com/products/app-engine">AppEngine</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.github.com">GitHub</button><button class="btn-right" type="submit" form="menu" formaction="https://chunkhost.com/r/waptug722">VPS</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.apachefriends.org/en/index.html">XAMPP</button>
+	<p style="font-size:10pt;">Web Application Programming Technology Training Resources</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://code.org/">Code.Org</button><button class="btn-mid" type="submit" form="menu" formaction="http://codility.com/">Codility.Com</button><button class="btn-mid" type="submit" form="menu" formaction="http://csedweek.org">Computer Science Education Week</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.w3schools.com">W3 Schools</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compileonline.com">CompileOnline</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.netlit101.com">Net Lit 101</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.codechef.com/school">CodeChef</button><button class="btn-right" type="submit" form="menu" formaction="https://www.khanacademy.org/">Khan Academy</button>
+	<p style="font-size:10pt;">Web Application Programming Tools</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://www.jsfiddle.net">jsFiddle</button><button class="btn-mid" type="submit" form="menu" formaction="http://sta.sh/muro/">Draw On-Line</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.picmonkey.com">Edit Photos Online</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.phpclasses.org">PHP Classes</button><button class="btn-mid" type="submit" form="menu" formaction="http://doublesvsoop.sourceforge.net/">PWCT-Make Windows Applications</button><button class="btn-right" type="submit" form="menu" formaction="http://www.jsclasses.org">JavaScript Classes</button>
+	<p style="font-size:10pt;">Project Partners</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://bit.ly/EastLakeWebDevGroup">Church</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.waptug.org">Educational</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.joomlaforgeeks.com">Community</button><button class="btn-right" type="submit" form="menu" formaction="http://www.geekzonebooks.com">Industry</button>
+	<p style="font-size:10pt;">Web Site Design Project Ideas</p>
+	<button class="btn-left" type="submit" form="menu" formaction="http://webdesignerwall.com/trends/single-page-portfolio-sites">Single Page Designs</button><button class="btn-mid" type="submit" form="menu" formaction="http://innovate.blurgroup.com/index.html">Plan your project</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.kickstarter.com/">Fund your project</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.gumroad.com/GeekZoneBooks">Sell your project</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compute.org">Seed money for your project</button><button class="btn-right" type="submit" form="menu" formaction="http://www.codiad.com">CODIAD Web IDE</button>
+	<p><iframe width="560" height="315" src="//www.youtube.com/embed/Umo_CXry6rY" frameborder="0" allowfullscreen></iframe></p>
+	<!-- Social Buttons -->
+<p>Help us reach more of the internet. Please share with your friends.</br>
 <div class="g-plus" data-action="share" data-height="15"></div>
 <!-- Place this code where you want the google badge to render. -->
 <a href="//plus.google.com/115522322296550875517?prsrc=3"
@@ -103,51 +160,50 @@ $("#hidemenu").hide();
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>
+
 <!-- End Social Buttons -->
- <img src="../../../logo.png">
- <textarea id="about" rows="5" readonly>
-About Us
+<!--Hitcounter start-->
+<a href="http://www.easycounter.com/">
+<img src="http://www.easycounter.com/counter.php?onlineinteractivity"
+border="0" alt="Unique Visitors from Sept 26,2013">Visitors</a> 
+<!--Hitcounter end-->
+<!-- HypeStat code -->
+<a href="http://on-line-interactivity.com.hypestat.com" title="Powered by HypeStat.com" target="_blank"><img src="http://www.hypestat.com/widget.php?w=160&h=150&pl=10&pt=20&c1=68C0AF&c2=000000&gd=vertical&f=stan0752&fs=6&fc=dddddd&f2=stan0765&fs2=6&fc2=FFFFFF&bc=404C4F&wt=pgyarw&url=on-line-interactivity.com" alt="" border="0"/></a>
+<!--End HypeStat code -->
+<!-- Copyright (c) 2013 GeekZoneBooks.Com -->
+</br>
+<pre>
+The MIT License (MIT)
 
-Welcome to the On-Line-Interactivity Web Based IDE Project
+Copyright (c) 2013 GeekZoneBooks.Com
 
-We invite you to apply for a new account if you have an interest in learning how to create web applications and web sites.
+Permission is hereby granted, free of 
+charge, to any person obtaining a copy
+of this software and associated documentation
+files (the "Software"), to deal in the 
+Software without restriction, including 
+without limitation the rights
+to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-This site is powered by the CODIAD Web IDE to allow you to create projects and edit the code with out needing any client desktop programming tools.
+The above copyright notice and this permission
+notice shall be included in all copies or 
+substantial portions of the Software.
 
-We have meetings the first Saturday of every month from 9 am to noon to share our projects and get help from other members. Members that want to use our on-line IDE tool will need to attend a meeting to have the access activated.
-
-We meet at Tullys in the Canyon Park Plaza in Bothell: 22615 Bothell Everett Hwy Bothell WA 98021
-
-This is a joint project between the following groups:
- Eastlake Community Church Web Design and Development Growth Group
- On-Line-Interactivity.Com  - Web Development Application IDE
- WapTug.Org  - Web Applications Programming Technology Users Group
- JoomlaForGeeks.Com - Joomla Users Group of Bothell
- GeekZoneBooks.Com - Web Hosting Services (Sponsor who is paying for the hosting space)
- </textarea>
-<div id="legallinks"><a href="http://www.waptug.org">About Us</a>|<a href="http://www.waptug.org/index.php?option=com_content&view=article&id=7">Terms of Service</a>|<a href="http://www.on-line-interactivity.com/WebIDE/index.php?">Login</a>|<a href="mailto:waptug@gmail.com">Contact Us</a>|<a href="../index.html">Stats</a>|<a href="http://on-line-interactivity.com/WapTugU/moodle">WapTug U</a>|<a href="http://bit.ly/GeekChatRoom">Support Chat</a></div>
-<div id="buttons">
-<button id="hidemenu">Hide Resource Menu</button>
-<button id="showmenu">Show Resource Menu</button>
-<button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN To WebIDE</button>
-<button class="btn-mid" type="submit" form="menu" formaction="http://waptug.github.io/on-line-interactivity/">View On GitHub</button>
-</div>
-</div>
-
-    <div id="container">
-    <p style "font-size:10pt;">Account Functions</p>        
-    <button class="btn-left" type="submit" form="menu" formaction="http://gg.eastlakecc.com/groups/1232" value="new">Request New Membership Account</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.on-line-interactivity.com/WebIDE/index.php">LOGIN</button><button class="btn-right" type="submit" form="menu" formaction="../members">Current Projects Directory</button>
-	<p style="font-size:10pt;">External Web Application Hosting Resources</p>
-	<button class="btn-left" type="submit" form="menu" formaction="http://www.touchdevelop.com">Mobile</button><button class="btn-mid" type="submit" form="menu" formaction="http://bit.ly/WapTugPaaSCloud">PaaS</button><button class="btn-mid" type="submit" form="menu" formaction="https://cloud.google.com/products/app-engine">AppEngine</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.github.com">GitHub</button><button class="btn-right" type="submit" form="menu" formaction="https://chunkhost.com/r/waptug722">VPS</button>
-	<p style="font-size:10pt;">Web Application Programming Technology Training Resources</p>
-	<button class="btn-left" type="submit" form="menu" formaction="http://code.org/">Code.Org</button><button class="btn-mid" type="submit" form="menu" formaction="http://codility.com/">Codility.Com</button><button class="btn-mid" type="submit" form="menu" formaction="http://csedweek.org">Computer Science Education Week</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.w3schools.com">W3 Schools</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compileonline.com">CompileOnline</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.netlit101.com">Net Lit 101</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.codechef.com/school">CodeChef</button><button class="btn-right" type="submit" form="menu" formaction="https://www.khanacademy.org/">Khan Academy</button>
-	<p style="font-size:10pt;">Web Application Programming Tools</p>
-	<button class="btn-left" type="submit" form="menu" formaction="http://www.jsfiddle.net">jsFiddle</button><button class="btn-mid" type="submit" form="menu" formaction="http://sta.sh/muro/">Draw On-Line</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.phpclasses.org">PHP Classes</button><button class="btn-mid" type="submit" form="menu" formaction="http://doublesvsoop.sourceforge.net/">PWCT-Make Windows Applications</button><button class="btn-right" type="submit" form="menu" formaction="http://www.jsclasses.org">JavaScript Classes</button>
-	<p style="font-size:10pt;">Project Partners</p>
-	<button class="btn-left" type="submit" form="menu" formaction="http://bit.ly/EastLakeWebDevGroup">Church</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.waptug.org">Educational</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.joomlaforgeeks.com">Community</button><button class="btn-right" type="submit" form="menu" formaction="http://www.mygeekzone.net">Industry</button>
-	<p style="font-size:10pt;">Web Site Design Project Ideas</p>
-	<button class="btn-left" type="submit" form="menu" formaction="http://webdesignerwall.com/trends/single-page-portfolio-sites">Single Page Designs</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.kickstarter.com/">Fund your project</button><button class="btn-mid" type="submit" form="menu" formaction="http://www.compute.org">Seed money for your project</button><button class="btn-right" type="submit" form="menu" formaction="http://www.codiad.com">CODIAD Web IDE</button>
-
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY
+OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+</pre>
+</p>
     </div>
 <form id="menu">
 			</form> 
